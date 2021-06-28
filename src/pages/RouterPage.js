7c98ee11b413292889fe243f370bc61c64d3983a
amontalvof <amontalvof92@gmail.com>
2021-06-28 12:01:26 -0500

    feat: router and signin screen

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -5,38 +5,61 @@ import {
     VideoCameraOutlined,
     UploadOutlined,
 } from '@ant-design/icons';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
+import { SignIn } from './SignIn';
+import { InLine } from './InLine';
+import { CreateTicket } from './CreateTicket';
+import { Desk } from './Desk';
 
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
     return (
-        <Layout style={{ height: '100vh' }}>
-            <Sider>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        Sign In
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                        In Line
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<UploadOutlined />}>
-                        Create Ticket
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: 280,
-                    }}
-                >
-                    Content
-                </Content>
+        <Router>
+            <Layout style={{ height: '100vh' }}>
+                <Sider collapsedWidth="0" breakpoint="md">
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                    >
+                        <Menu.Item key="1" icon={<UserOutlined />}>
+                            <Link to="/signin">Sign In</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                            <Link to="/inline">In Line</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<UploadOutlined />}>
+                            <Link to="/create">Create Ticket</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        <Switch>
+                            <Route path="/signin" component={SignIn} />
+                            <Route path="/inline" component={InLine} />
+                            <Route path="/create" component={CreateTicket} />
+                            <Route path="/desk" component={Desk} />
+                            <Redirect to="/signin" />
+                        </Switch>
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
+        </Router>
     );
 };
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,67 @@
 import React from 'react';
+import { Form, Input, InputNumber, Button } from 'antd';
+import { LoginOutlined } from '@ant-design/icons';
 
 export const SignIn = () => {
-    return <div>SignIn</div>;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 14,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Agent name"
+                name="agent"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your agent name!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Desk"
+                name="desk"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your desk number!',
+                    },
+                ]}
+            >
+                <InputNumber min={1} max={99} />
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 14,
+                }}
+            >
+                <Button type="primary" htmlType="submit" shape="round">
+                    <LoginOutlined />
+                    Sign in
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
